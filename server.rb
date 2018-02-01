cd5ce921704e3be4e7f3b6f3e8735127a141caf6
ruegen <ruegen@icloud.com>
2018-02-01 16:45:55 +1100

    add stripe

--- server.rb
+++ server.rb
@@ -1,6 +1,7 @@
 require 'webrick'
 require 'erb'
 require 'dotenv/load'
+require 'stripe'
 
 class Routes < WEBrick::HTTPServlet::AbstractServlet
 
@@ -25,7 +26,34 @@ class Routes < WEBrick::HTTPServlet::AbstractServlet
     def do_POST(request, response)
         case request.path
         when "/charge" 
-            puts "charge"  
+            token = request.body.split('=')[1]
+            ###
+            # configure stripe
+            Stripe.api_key = ENV["STRIPE_TEST_KEY"]
+
+            # # 1. Charges
+            stripe_response = Stripe::Charge.create({
+                :amount => 2000, #dollars in cents
+                :currency => "aud", # Australia currency
+                :source => "tok_amex", # obtained with Stripe.js
+                :description => "Charge for ruegen.aschenbrenner@coderacademy.edu.au"
+            })
+
+
+            html = """
+            <html>
+            <body>
+            <p>Description: #{stripe_response.description}</p>
+            <p>Amount: #{stripe_response.amount}</p>
+            <p>Currency: #{stripe_response.currency}</p>
+            </body>
+            </html>
+            """
+            response.status = 200
+            response['Content-Type'] = 'text/html'
+            response.body = html 
+
+            ###
         else
             response.status = 400
             response.body = "Route not found"
